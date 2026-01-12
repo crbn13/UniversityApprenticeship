2eb3c9a2a852f2cd3904d5cc9be2ee720f37e5bf
Rory Ainsworth <Rory.Ainsworth@yokogawa.com>
2026-01-12 14:40:55 +0000

    added private document submodule

--- submodule_pull_changes.sh
+++ submodule_pull_changes.sh
@@ -6,3 +6,10 @@ git fetch origin master
 git merge
  git commit -m " automated picture addition "
 git push origin master
+
+cd ../ApprenticeshipDocuments 
+git checkout main
+git fetch origin main
+git merge
+git commit -m " automated commit "
+git push origin main
